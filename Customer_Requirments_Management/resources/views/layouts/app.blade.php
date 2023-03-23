e96219936eac6f9212ea5b75fdbb2fc4ab33b14b
LAKSHAN J.A.T <91153687+LAKSHAN-JAT@users.noreply.github.com>
2023-03-23 14:05:19 +0530

    implement dashboard

---
+++ Customer_Requirments_Management/resources/views/layouts/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <meta charset="UTF-8">
+ 
+        <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css')}}">
+
+  <!-- CSS Libraries -->
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/css/components.css')}}">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+
+<body>
+    
+<div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+    @include('layouts.header')
+
+    @include('layouts.sidebar')
+
+    @yield('content')
+
+    @include('layouts.footer')
+
+
+    </div>
+    </body>
+   
+</html>
---
+++ Customer_Requirments_Management/resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,22 @@
+<div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="#" style="font-size:x-small">Food Distributions System</a>
+            <img alt="image" src="assets/img/avatar/1.webp" class="shadow p-3 mb-5 bg-body rounded-circle" height="100px" width="100px">
+          </div>
+          <div class="sidebar-brand sidebar-brand-sm">
+            <a href="#" style="font-size:x-small">FDS</a>
+            <img alt="image" src="assets/img/avatar/1.webp" class="shadow p-3 mb-5 bg-body rounded-circle" height="70px" width="70px">
+          </div>
+          <br/><br/>  <br/>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Main Manu</li>
+            <li><a class="nav-link" href="#"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+            <li class="menu-header">Customer</li>
+            <li class="dropdown">
+             <li><a class="nav-link" href="#"><i class="fas fa-table"></i> <span>Requirements</span></a></li>
+            <li><a class="nav-link" href="#"><i class="fas fa-user"></i> <span>Demo</span></a></li>
+            <li><a class="nav-link" href="#"><i class="fas fa-car"></i> <span>Demo</span></a></li>
+            </li>
+        </aside>
+      </div>
