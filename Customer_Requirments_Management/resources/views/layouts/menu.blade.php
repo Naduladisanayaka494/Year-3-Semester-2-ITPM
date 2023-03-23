95eb0cb93444bd282f7c5e1d084107da926a183d
LAKSHAN J.A.T <91153687+LAKSHAN-JAT@users.noreply.github.com>
2023-03-23 12:14:18 +0530

    delete project

--- Customer_Requirments_Management/resources/views/layouts/menu.blade.php
+++
@@ -1,7 +0,0 @@
-<li class="menu-header">Main Menu</li>
-<li><a class="nav-link" href="#"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
-<li><a class="nav-link" href="#"><i class="fas fa-user"></i> <span>Admins</span></a></li>
-<li><a class="nav-link" href="#"><i class="fas fa-car"></i> <span>Vehicles</span></a></li>
-<li><a class="nav-link" href="#"><i class="fas fa-users"></i> <span>Staff</span></a></li>
-<li><a class="nav-link" href="#"><i class="fas fa-user-tie"></i> <span>Visitors</span></a></li>
-<li><a class="nav-link" href="#"><i class="fas fa-user-nurse"></i><span>Drivers</span></a></li>
