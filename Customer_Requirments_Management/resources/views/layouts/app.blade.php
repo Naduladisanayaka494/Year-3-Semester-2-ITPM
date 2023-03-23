2a32867ccf3e6ba335c1f85c2360e9b435648f2e
LAKSHAN J.A.T <91153687+LAKSHAN-JAT@users.noreply.github.com>
2023-03-23 11:19:12 +0530

    footer

--- Customer_Requirments_Management/resources/views/layouts/app.blade.php
+++ Customer_Requirments_Management/resources/views/layouts/app.blade.php
@@ -6,16 +6,16 @@
   <title>Blank Page &mdash; Stisla</title>
 
   <!-- General CSS Files -->
-  <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css')}}">
 
   <!-- CSS Libraries -->
    <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
   <!-- Template CSS -->
-  <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="assets/css/components.css">
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/css/components.css')}}">
 <!-- Start GA -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
 <script>
@@ -29,6 +29,19 @@
 </head>
 
     <body>
+<div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+
+
+      @include('layouts.header')
+      @include('layouts.sidebar')
+      @yield('content')
+      @include('layouts.footer')
+
+
+    </div>
+  </div>
 
 
 
