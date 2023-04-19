98694f565532a044a84b632a4a61e4e9d50ea9ad
LAKSHAN J.A.T <91153687+LAKSHAN-JAT@users.noreply.github.com>
2023-04-19 10:31:57 +0530

    Revert "login implementation"
    This reverts commit 5f0943a7379d238173cc0846b1309e26bed705d1.

--- Requirements_Management/routes/web.php
+++ Requirements_Management/routes/web.php
@@ -24,7 +24,3 @@ Route::resource('/add-req', RequirementscreateController::class);
 Route::resource('/requirements', RequirementsController::class);
 
 
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
