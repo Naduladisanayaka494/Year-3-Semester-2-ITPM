55490b2f8b7cf0c9e10ba7cadb13d4e72eef720f
LAKSHAN J.A.T <91153687+LAKSHAN-JAT@users.noreply.github.com>
2023-04-19 21:58:34 +0530

    routes

--- Requirements_Management/resources/views/layout/header.blade.php
+++ Requirements_Management/resources/views/layout/header.blade.php
@@ -18,7 +18,7 @@
                                     {{ Auth::user()->name }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu dropdown-menu-end col-md-6" aria-labelledby="navbarDropdown" >
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
--- Requirements_Management/resources/views/user-dashboard.blade.php
+++ Requirements_Management/resources/views/user-dashboard.blade.php
@@ -1,87 +1,40 @@
-
 @extends('layout.app')
 
 @section('content')
 
 <section class="section">
+
           <div class="section-header">
             <ul>
-            <h1><i class="fa-solid fa-table"></i> &nbsp;  Customers' Requirements</h1>
+            <h1><i class="fa-solid fa-house"></i> &nbsp;Dashboard</h1>
             </ul>
           </div>
 
-           <!-- START: Breadcrumbs-->
-                <div class="row ">
-                    <div class="col-12  align-self-center">
-                        <div class="sub-header mt-3 py-3 px-3 align-self-center d-sm-flex w-100 rounded">
-                            <div class="w-sm-100 mr-auto"><h4 class="mb-0">Requirements Information</h4></div>
-                         </div>
-                    </div>
-                    
-                </div>
-                <!-- END: Breadcrumbs-->
-     <!-- START: Main Content-->
-        <main>
-            <div class="container-fluid">
-                                <!-- START: Card Data-->
+                   <!-- START: Card Data-->
                 <div class="row">
-                    <div class="col-12 mt-3">
-                        <div class="card">
+                    <div class="col-12 col-sm-6 col-xl-3 mt-3">
+                        <div class="card shadow p-3 mb-5 bg-body rounded">
                             <div class="card-body">
-                                <div class="table-responsive">
-                                    <table id="example" class="display table dataTable table-striped table-bordered">
-                                        <thead>
-                                            <tr>
-                                             
-                                                <th>DEMO1</th>
-                                                <th>DEMO2</th>
-                                                <th>DEMO3</th>
-                                                <th>DEMO4</th>
-                                                <th>DEMO5</th>
-                                                <th>DEMO6</th>
-                                                <th>DEMO7</th>
-                                                <th>DEMO8</th>
-                                                <th width = "200">Manage</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                             @foreach($Requirements as $item)
-                                             <tr>
-                                             
-                                                
-                                                <input type="hidden" class="reqdelete_val_id" value="{{$item->id}}">
-                                                <td>{{ $item->DEMO1 }}</td>
-                                                <td>{{ $item->DEMO2 }}</td>
-                                                <td>{{ $item->DEMO3 }}</td>
-                                                <td>{{ $item->DEMO4 }}</td>
-                                                <td>{{ $item->DEMO5 }}</td>
-                                                <td>{{ $item->DEMO6 }}</td>
-                                                <td>{{ $item->DEMO7 }}</td>
-                                                 <td>{{ $item->DEMO8 }}</td>
-                                                    <td>
-                                                        <a href="{{ url('/requirements/' . $item->id . '/edit') }}"  class="btn btn-primary"> <i class="fa fa-edit"></i>&nbsp; Edit</a>
-                                                         <form method="POST" action="{{ url('/requirements' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button type="submit" class="confirm_del_btn btn btn-danger"  onclick="return confirm(Confirm)"><i class="fa fa-trash"></i> &nbsp;Delete</button>
-                                            </form>
-                                                                                                                                                                                                                   
-                                                    </td>
-                                            </tr>
-                                             @endforeach
-                                        </tbody>  
-                                    </table>
-                                </div>
-                            </div>
-                        </div> 
+                                <img src="{{ asset('/template/template/assets/img/avatar/Users.jpg') }}" class="rounded-circle-lg" alt="admins" class="float-right" width="50" height="50" align = "right">
+                                <h5 class="card-title font-weight-bold">Customers' Requirements</h5> &nbsp;
+                                   <h5 class="card-subtitle mb-2 text-muted"><a href="{{url('requirements')}}">Show</a> &nbsp;<i class="fa-solid fa-hand-point-left fa-beat" style= "color: #011ffe;"></i></h5>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">
 
-                    </div>                  
-                </div>
-                <!-- END: Card DATA-->
-            </div>
-        </main>
-        <!-- END: Content-->
-          
+                                 <?php
+
+                                 $connection = mysqli_connect("localhost","root","","food_distribution_system_db","3307");
+                                 $query = "SELECT id FROM requirements ORDER BY id";
+                                 $query_run = mysqli_query($connection, $query);
+                                 $row = mysqli_num_rows($query_run);
+
+                                 echo '<h1> '.$row.'</h1>';
+                                 
+                                 ?>
+
+                                 </div>
+                            </div>
+                        </div>
+                    </div>
 </section>
 
 @endSection
