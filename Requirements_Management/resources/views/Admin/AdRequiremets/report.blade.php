fc52d48fb831c59ece2c696c930678e2b0673533
LAKSHAN J.A.T <91153687+LAKSHAN-JAT@users.noreply.github.com>
2023-05-17 15:59:36 +0530

    report edited

--- Requirements_Management/resources/views/Admin/AdRequiremets/report.blade.php
+++ Requirements_Management/resources/views/Admin/AdRequiremets/report.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <h1>{{ $requirement->customer_name}}</h1>
+
+
+
+  <!-- <h1>{{ $requirement->customer_name}}</h1>
    <h1>{{ $requirement->address}}</h1>
     <h1>{{ $requirement->email}}</h1>
      <h1>{{ $requirement->phone}}</h1>
@@ -17,6 +11,4 @@
    <h1>{{ $requirement->Quantity}}</h1>
     <h1>{{ $requirement->pack_preference}}</h1>
      <h1>{{ $requirement->preferred_day}}</h1>
-   <h1>{{ $requirement->preferred_time}}</h1>
-</body>
-</html>
+   <h1>{{ $requirement->preferred_time}}</h1> -->
