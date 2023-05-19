2598ab7715d9c070a94990d6b93cdc9fa16b6cd3
nadula.fourdiv <nadula.fourdiv@gmail.com>
2023-05-20 01:30:07 +0530

    changes in the report

--- resources/views/products/report.blade.php
+++ resources/views/products/report.blade.php
@@ -3,134 +3,126 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Monthly Feedback Report</title>
     <style>
-       
-        h1,h2,h3,h4,h5,h6{
+        /* Add your custom styles here */
+
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
             margin: 0;
             padding: 0;
-        }
-        p{
-            margin: 0;
-            padding: 0;
-        }
-        .container{
-            width: 105%;
-            margin-right: auto;
-            margin-left: auto;
-        }
-        .brand-section{
-           background-color: #0d1033;
-           padding: 10px 40px;
-        }
-        .logo{
-            width: 50%;
         }
 
-        .row{
-            display: flex;
-            flex-wrap: wrap;
-        }
-        .col-6{
-            width: 50%;
-            flex: 0 0 auto;
-        }
-        .text-white{
-            color: #fff;
-        }
-    
-        .body-section{
-            padding: 16px;
-            border: 1px solid gray;
-        }
-        .heading{
-            font-size: 20px;
-            margin-bottom: 08px;
-        }
-        .sub-heading{
-            color: #262626;
-            margin-bottom: 05px;
-        }
-        table{
+        .container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
             background-color: #fff;
-            width: auto;
+            border: 1px solid #ccc;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .logo {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+            margin: 0;
+            padding: 0;
+            font-family: 'Courier New', Courier, monospace;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .text-right {
+            text-align: right;
+        }
+
+        .table {
+            width: 100%;
             border-collapse: collapse;
         }
-        table thead tr{
-            border: 1px solid #111;
+
+        .table thead {
             background-color: #f2f2f2;
         }
-        table td {
-            vertical-align: middle !important;
-            text-align: left;
+
+        .table th, .table td {
+            padding: 10px;
+            text-align: center;
         }
-        table th, table td {
-            padding-top: 08px;
-            padding-bottom: 08px;
+
+        .table-bordered th,
+        .table-bordered td {
+            border: 1px solid #ccc;
         }
-        .table-bordered{
-            box-shadow: 0px 0px 8px 0.5px gray;
+
+        .brand-section {
+            background-color: #0d1033;
+            padding: 10px;
         }
-        .table-bordered td, .table-bordered th {
-            border: 1px solid #dee2e6;
+
+        .brand-section h1 {
+            color: #fff;
+            font-size: 24px;
+            margin-bottom: 0;
         }
-        .text-right{
-            text-align: end;
+
+        .footer {
+            margin-top: 20px;
+            color: #777;
+            font-size: 12px;
         }
-        .w-20{
-            width: 20%;
+
+        .footer a {
+            color: #777;
+            text-decoration: none;
         }
-        .float-right{
-            float: right;
+
+        .footer a:hover {
+            text-decoration: underline;
         }
     </style>
 </head>
 <body>
-    <br/>
-   <h3 style="font-family: 'Courier New', Courier, monospace;"><center>FOOD TO YOUR DOORSTEP</center></h3><br/>
-
-<center><img src="{{ public_path('/template/template/assets/img/avatar/logo.png' ) }}"  alt="Image" width="100" class="rounded-circle"></center>
-   <br/><br/>
     <div class="container">
         <div class="brand-section">
-            <div class="row">
-                <div class="col-6">
-                    <h1 class="text-white">Monthly Feedback Report</h1>
-                </div>
-            </div>
+            <h1>Monthly Feedback Report</h1>
         </div>
-        
-        <div class="body-section">
-            <h3 class="heading" style="text-align: center;">Monthly Report Of Product</h3>
-            <br>
-            <table class="table-bordered">
-                <thead>
-                    <tr>
-                    <th style="font-family: 'Courier New', Courier, monospace; font-size:small" ><center>Product Name</center></th>
-                        <th style="font-family: 'Courier New', Courier, monospace; font-size:small" ><center>Price</center></th>
-                        <th style="font-family: 'Courier New', Courier, monospace; font-size:small" ><center>Quantity</center></th>
-          
-                                                 
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($vegetable as $item)
-                    <tr>
-                   
-               <td style="font-family: 'Courier New', Courier, monospace; font-size:small"><center>{{ $item->pname }}</center></td>
-               <td style="font-family: 'Courier New', Courier, monospace; font-size:small"><center>{{ $item->price }}</center></td>
-               <td style="font-family: 'Courier New', Courier, monospace; font-size:small"><center>{{ $item->qty }}</center></td>
-            </tr>   
-                            @endforeach
-                           
-                                </tbody>
-            </table>
-            <br>
-        
-            <p>&copy; 2022 All Rights Reserved. 
-                <a href="#" class="float-right">www.superfashion.com</a>
-            </p>
-            
-    </div>      
+
+        <div class="logo">
+            <img src="{{ public_path('/template/template/assets/img/avatar/logo.png') }}" alt="Logo" width="100" class="rounded-circle">
+        </div>
+
+        <h3 class="text-center">Monthly Report Of Product</h3>
+
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($vegetable as $item)
+                <tr>
+                    <td>{{ $item->pname }}</td>
+                    <td>{{ $item->price }}</td>
+                    <td>{{ $item->qty }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="footer">
+            &copy; {{ date('Y') }} All Rights Reserved. 
+            <a href="#">www.superfashion.com</a>
+        </div>
+    </div>
 </body>
 </html>
