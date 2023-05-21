2f07405b6529681088aa69373b646fec3e8309ec
LAKSHAN J.A.T <91153687+LAKSHAN-JAT@users.noreply.github.com>
2023-05-21 12:20:47 +0530

    delete main content

--- Requirements_Management/storage/framework/views/efe7cb960f2f099eb97ed5548fe8d18c3f97d608.php
+++
@@ -1,6 +0,0 @@
-<li class="menu-header">Main Menu</li>
-<li><a class="nav-link" href="<?php echo e(url('dashboard')); ?>"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
-<li class="menu-header">Customer Section</li>
-<li><a class="nav-link" href="<?php echo e(url('requirements')); ?>"><i class="fas fa-table"></i> <span>Requirements Manage</span></a></li>
-
-<?php /**PATH D:\Year-3-Semester-2-ITPM\Requirements_Management\resources\views/layout/menu.blade.php ENDPATH**/ ?>
